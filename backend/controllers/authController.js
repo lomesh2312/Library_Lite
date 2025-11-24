0400babb9b6fe65c4ac6edc748cf61ab9b5092a3
Manik Tyagi <tyagimanik77@gmail.com>
2025-11-24 19:26:20 +0530

    authController

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,37 +1,58 @@
-const prisma = require('../prismaClient');
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
+const prisma = require("../prismaClient");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 const register = async (req, res, next) => {
+    try {
+        const {name, email, password} = req.body;
 
-  try {
-    const { name, email, password } = req.body
-    const hash = await bcrypt.hash(password, 10)
-    const user = await prisma.user.create({ data: { name, email, passwordHash: hash } })
-    res.json({ id: user.id, name: user.name, email: user.email })
-  } catch (e) {
-    next(e)
-  }
+        const hash = await bcrypt.hash(password, 10)
+        const user = await prisma.user.create({ data: {name, email, passwordHash: hash}})
+        
+        res.json({id: user.id, name: user.name, email: user.email})
+
+    } catch (e) {
+        next(e)
+    }
 }
 
-
 const login = async (req, res, next) => {
-  try {
-    const { email, password } = req.body
-    const user = await prisma.user.findUnique({ where: { email } })
-    if (!user) return res.status(401).json({ error: 'invalid' })
-    const ok = await bcrypt.compare(password, user.passwordHash)
-    if (!ok) return res.status(401).json({ error: 'invalid' })
-    const token = jwt.sign({ userId: user.id, isAdmin: user.isAdmin }, process.env.JWT_SECRET, { expiresIn: '1h' })
-    res.json({ accessToken: token, user: { id: user.id, name: user.name, email: user.email, isAdmin: user.isAdmin } })
-  } catch (e) {
-    next(e)
-  }
+    try {
+        const {email, password } = req.body
+
+        const admin = await prisma.admin.findUnique({ where: { email_id: email }})
+
+        if (!admin) {
+            return res.status(401).json({ error: "You are not the Librarian" })
+        }
+
+        const ok = await bcrypt.compare(password, admin.password)
+        if (!ok) return res.status(401).json({ error: "Invalid credentials" })
+
+            const token = jwt.sign({ userId: admin.admin_id, isAdmin: true }, process.env.JWT_SECRET || 'secret', {expiresIn: '1h'})
+
+            res.json({
+                accessToken: token,
+                user: {
+                    id: admin.admin_id,
+                    name: admin.name,
+                    email: admin.email_id,
+                    isAdmin: true
+                }
+            })
+    } catch (e) {
+        next(e)
+    }
 }
 
 const exist = async (req, res) => {
-  const user = await prisma.user.findUnique({ where: { id: req.user.userId } })
-  res.json({ id: user.id, name: user.name, email: user.email, isAdmin: user.isAdmin })
+    const user = await prisma.user.findUnique({
+        where: {
+            id: req.user.userId
+        }
+    })
+
+    res.json({ id: user.id, name: user.name, email: user.email, isAdmin: user.isAdmin })
 }
 
 module.exports = { register, login, exist }
