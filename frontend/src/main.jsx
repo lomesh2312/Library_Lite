a51e5f0a71ca3d32dcd9ca2ae12d0d0aa53a4b55
Lomesh Sonkeshriya <luckysonkeshriya@Lomeshs-MacBook-Pro.local>
2025-12-01 23:19:28 +0530

    frontend/app

---
+++ frontend/src/main.jsx
@@ -0,0 +1,51 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import axios from 'axios';
+
+
+axios.defaults.baseURL = 'http://localhost:4000';
+
+
+axios.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem('token');
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
+
+
+axios.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    
+    if (error.response && error.response.status === 401) {
+
+      if (!window.location.pathname.includes('/login')) {
+
+        localStorage.removeItem('token');
+        window.location.href = '/login';
+      }
+    }
+    return Promise.reject(error);
+
+  }
+
+);
+import { ThemeProvider } from './context/ThemeContext';
+
+createRoot(document.getElementById('root')).render(
+
+  <StrictMode>
+    <ThemeProvider>
+      <App />
+
+    </ThemeProvider>
+
+  </StrictMode>,
+)
