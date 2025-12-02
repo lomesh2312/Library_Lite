72ff81c2b665413f234f494585fb3dae01559f4c
lomesh2312 <lomesh.sonkeshriya@adypu.edu.in>
2025-12-02 15:22:31 +0530

    added allowedHosts

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  preview:{ allowedHosts: ['library-lite-1.onrender.com'],}
 });
